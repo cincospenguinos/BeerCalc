b85171b0a8612040cc8789c38914a1d38c3985aa
Andre LaFleur <andre.lafleur@utah.edu>
2019-06-05 19:03:07 -0600

    Setup proper frame styles

--- webpack/app.jsx
+++ webpack/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import FermentablesFrame from "./components/frame/fermentables-frame";
 
+import styles from './styles.css';
+
 export default function App() {
   return (
-    <div>
+    <div className={styles.container}>
       <FermentablesFrame />
     </div>
   );
